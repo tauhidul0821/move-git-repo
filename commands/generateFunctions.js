bc0e24cd279dcc25022635b4197c696cdc3ae05b
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-04-20 09:36:04 +0600

    template genereted

--- commands/generateFunctions.js
+++ commands/generateFunctions.js
@@ -10,15 +10,14 @@ const generateFunctions = {
   genComponent() {
     const cwd = process.cwd();
     const file_name = process.argv[3];
-    // console.log('generate component at ', cwd);
+    const sampleComponentPath = path.join(componentPath, "/sample.component");
+    const originalContent = fs.readFileSync(sampleComponentPath, "utf8");
+
     /* 
-
     jodi file name er age / [slash ] thake then cwd er sathe location concat hobe 
-
     ex: bs g c components/name_of_component
     1. if not exits components folder, create components folder
     2. if exits components folder then create file inside components folder
-
     */
 
     if (file_name.includes("/")) {
@@ -36,24 +35,18 @@ const generateFunctions = {
           if (error) {
             console.error(error);
           } else {
-            const sampleComponentPath = path.join(componentPath, "/sample.component");
-            const originalContent = fs.readFileSync(sampleComponentPath, "utf8");
             // replace content
             const replacedContent = originalContent.replace(componentRegExp, `${fileName}`);
             fs.writeFileSync(`${cwd}/${file_name}.js`, replacedContent);
           }
         });
       } else if (fs.existsSync(dir)) {
-        const sampleComponentPath = path.join(componentPath, "/sample.component");
-        const originalContent = fs.readFileSync(sampleComponentPath, "utf8");
         // replace content
         const replacedContent = originalContent.replace(componentRegExp, `${fileName}`);
         fs.writeFileSync(`${cwd}/${file_name}.js`, replacedContent);
 
       }
     } else {
-      const sampleComponentPath = path.join(componentPath, "/sample.component");
-      const originalContent = fs.readFileSync(sampleComponentPath, "utf8");
       // replace content
       const replacedContent = originalContent.replace(componentRegExp, `${file_name}`);
       fs.writeFileSync(`${cwd}/${file_name}.js`, replacedContent);
@@ -63,6 +56,9 @@ const generateFunctions = {
     const cwd = process.cwd(); // find current working directory 
     const file_name = process.argv[3]; // find name of component
 
+    const sampleTemplatePath = path.join(templatePath, "/sampleTemplateFile.template");
+    const originalContent = fs.readFileSync(sampleTemplatePath, "utf8");
+
     if (file_name.includes("/")) {
       console.log('user need create folder ');
       var d = file_name.split("/")
@@ -71,8 +67,6 @@ const generateFunctions = {
       console.log('create_folder ->', dir);
       var fileName = d[d.length - 1];
       console.log('fileName -> ', fileName);
-      const sampleTemplatePath = path.join(templatePath, "/sampleTemplateFile.template");
-      const originalContent = fs.readFileSync(sampleTemplatePath, "utf8");
 
       // folder create success
       if (!fs.existsSync(dir)) {
@@ -94,7 +88,7 @@ const generateFunctions = {
 
     } else {
       // replace content
-      const replacedContent = originalContent.replace(templateRegExp, `${fileName}`);
+      const replacedContent = originalContent.replace(templateRegExp, `${file_name}`);
       fs.writeFileSync(`${cwd}/${file_name}.html`, replacedContent);
     }
   }
