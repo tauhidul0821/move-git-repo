10aec44df7351c74b35ca0580584342745bf91a4
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-04-20 09:15:27 +0600

    template generated successfuly

--- commands/generateFunctions.js
+++ commands/generateFunctions.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require("path");
 const sampleComponent = require('../generateSample/component/sampleComponent');
 const componentPath = path.join(__dirname, '../generateSample/component');
+const templatePath = path.join(__dirname, '../generateSample/template');
 const componentRegExp = /YourComponentName/g;
-// const sampletemplate = require('../generateSample/template/sampleTemplate.js');
+const templateRegExp = /YourTemplateName/g;
 
 const generateFunctions = {
   genComponent() {
@@ -59,22 +60,43 @@ const generateFunctions = {
     }
   },
   genTemplate() {
-    const cwd = process.cwd();
-    const file_name = process.argv[3];
-    console.log('generate component at ', cwd);
-    /* 
+    const cwd = process.cwd(); // find current working directory 
+    const file_name = process.argv[3]; // find name of component
 
-    jodi file name er age / [slash ] thake then cwd er sathe location concat hobe 
+    if (file_name.includes("/")) {
+      console.log('user need create folder ');
+      var d = file_name.split("/")
+      var folder_name_prefix = d.slice(0, d.length - 1);
+      var dir = folder_name_prefix.join('/');
+      console.log('create_folder ->', dir);
+      var fileName = d[d.length - 1];
+      console.log('fileName -> ', fileName);
+      const sampleTemplatePath = path.join(templatePath, "/sampleTemplateFile.template");
+      const originalContent = fs.readFileSync(sampleTemplatePath, "utf8");
 
-    ex: bs g c components/name_of_component
-    1. if not exits components folder, create components folder
-    2. if exits components folder then create file inside components folder
+      // folder create success
+      if (!fs.existsSync(dir)) {
+        fs.mkdir(`${cwd}/${dir}`, { recursive: true }, (error) => {
+          if (error) {
+            console.error(error);
+          } else {
+            // replace content
+            const replacedContent = originalContent.replace(templateRegExp, `${fileName}`);
+            fs.writeFileSync(`${cwd}/${file_name}.html`, replacedContent);
+          }
+        });
+      } else if (fs.existsSync(dir)) {
+        // replace content
+        const replacedContent = originalContent.replace(templateRegExp, `${fileName}`);
+        fs.writeFileSync(`${cwd}/${file_name}.html`, replacedContent);
 
-    */
-    const templatefile = path.join(templatePath, '/templateFile.template');
-    fs.writeFileSync(cwd + `/${file_name}.html`, templatefile);
+      }
 
-    console.log('file name and location', file_name)
+    } else {
+      // replace content
+      const replacedContent = originalContent.replace(templateRegExp, `${fileName}`);
+      fs.writeFileSync(`${cwd}/${file_name}.html`, replacedContent);
+    }
   }
 }
 module.exports = generateFunctions;
