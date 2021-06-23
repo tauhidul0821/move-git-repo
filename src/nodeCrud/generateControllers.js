82a219bae989d481a260cde01650404e61e2a487
Md. Tauhidul Islam <tauhidul.islam@brainstation23.com>
2021-06-23 15:49:31 +0600

    now directory manager create

--- src/nodeCrud/generateControllers.js
+++ src/nodeCrud/generateControllers.js
@@ -1,24 +1,10 @@
 const { create } = require('../../lib');
 
-const sampleText = `Lorem Ipsum1 is simply dummy text of the printing and typesetting industry. 
-
-Lorem Ipsum2 has been the industry's standard dummy text ever since the 1500s, 
-
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-
-It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-
-It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum3 passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum4.
-`
+const sampleText = `Generate controllers change to khadi`
 
 const fileName = `coursesControllers.js`;
 
-const replaceWord = `
-Ipsum1=${process.argv[2]},
-Ipsum2=Khadija,
-Ipsum3=${process.argv[3]},
-Ipsum4=Mamun
-`;
+const replaceWord = ``;
 
 const directory = `nodecrud/controllers`;
 
